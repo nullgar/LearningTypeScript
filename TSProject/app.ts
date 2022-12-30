9464a829b60e61b102c1b6d22eeead0d57a97fff
nullgar <technicianaguilar@gmail.com>
2022-12-29 23:12:14 -0800

    Used additional types to learn a bit more about Typescript

--- TSProject/app.ts
+++ TSProject/app.ts
@@ -1,1 +1,20 @@
-console.log("Code goes here!")
+const addTwoNums = (n1: number, n2: number, showResult: boolean, stringResult: string) => {
+   
+    const result = n1 + n2;
+    if (booleanResult) {
+
+        return stringResult + result;
+
+    } else {
+        console.log("There was an error.")
+    }
+};
+
+const theNum1 = 435567;
+const theNum2 = 44522;
+const booleanResult = true;
+const stringResult = "The reult is: ";
+
+const result = addTwoNums(theNum1, theNum2, booleanResult, stringResult);
+console.log(result);
+//export{};
