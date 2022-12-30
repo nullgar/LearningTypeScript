227a6c014fe981f07f6037eb169cb69d9e713ec9
nullgar <technicianaguilar@gmail.com>
2022-12-29 23:54:47 -0800

    Learned about nested object

--- TSProject/app.ts
+++ TSProject/app.ts
@@ -6,11 +6,28 @@ const person: {
 	age: 10000
 };
 
-const pc: object = {
-    type: "mac",
-    armChip: "M2",
-    cost: 4000
-}
+const car: {
+    make: string,
+    year: number,
+    wheels: {
+        tires: number,
+        make: string
+    }
+} = {
+    make: "Ford",
+    year: 2017,
+    wheels: {
+        tires: 4,
+        make: "Bridgestone"
+    }
+};
+
+// const pc: object = {
+//     type: "mac",
+//     armChip: "M2",
+//     cost: 4000
+// }
 
 console.log(person.age);
-console.log(pc.cost);
+//console.log(pc["cost"]); //This does not work you need to be specific for object to work properly.
+console.log(car);
