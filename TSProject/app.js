23bb5e67c32d64356e56a63a2550246b48cd741d
nullgar <technicianaguilar@gmail.com>
2023-01-01 22:48:52 -0800

    Finished any lecture

--- TSProject/app.js
+++ TSProject/app.js
@@ -11,11 +11,29 @@ var car = {
     },
     owners: ["nullgar", "frugalPhoenix", "V"]
 };
+var tupleCar = {
+    make: "Subaru",
+    year: 2015,
+    wheels: {
+        tires: 4,
+        make: "Bridgestone"
+    },
+    owners: ["nullgar", "frugalPhoenix", "V"],
+    accidents: [0, "None"] //Tells Typescript that this is a speacial array with a fixed length and fixed types
+};
 // const pc: object = {
 //     type: "mac",
 //     armChip: "M2",
 //     cost: 4000
 // }
+var Role;
+(function (Role) {
+    Role[Role["DADA"] = 0] = "DADA";
+    Role[Role["MAMA"] = 1] = "MAMA";
+    Role[Role["BUBBY"] = 2] = "BUBBY";
+})(Role || (Role = {}));
+;
+var networth = 1000;
 console.log(person.age);
 //console.log(pc["cost"]); //This does not work you need to be specific for object to work properly.
 console.log(car);
@@ -24,3 +42,5 @@ for (var _i = 0, _a = car.owners; _i < _a.length; _i++) {
     var owner = _a[_i];
     console.log(owner.toUpperCase()[0] + owner.slice(1));
 }
+;
+console.log('this', tupleCar.accidents);
--- TSProject/app.ts
+++ TSProject/app.ts
@@ -25,11 +25,36 @@ const car: {
     owners: ["nullgar", "frugalPhoenix", "V"]
 };
 
+const tupleCar: {
+    make: string,
+    year: number,
+    wheels: {
+        tires: number,
+        make: string,
+        
+    },
+    owners: string[],
+    accidents: [number, string]
+} = {
+    make: "Subaru",
+    year: 2015,
+    wheels: {
+        tires: 4,
+        make: "Bridgestone"
+    },
+    owners: ["nullgar", "frugalPhoenix", "V"],
+    accidents: [0, "None"] //Tells Typescript that this is a speacial array with a fixed length and fixed types
+}
+
 // const pc: object = {
 //     type: "mac",
 //     armChip: "M2",
 //     cost: 4000
 // }
+
+enum Role { DADA, MAMA, BUBBY };
+
+const networth: any = 1000;
 
 console.log(person.age);
 //console.log(pc["cost"]); //This does not work you need to be specific for object to work properly.
@@ -37,4 +62,6 @@ console.log(car);
 //let owner: string;
 for (const owner of car.owners) {
     console.log(owner.toUpperCase()[0] + owner.slice(1))
-}
+};
+
+console.log('this',tupleCar.accidents);
