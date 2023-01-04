64118fec867e2d48b2281e68ceb530b0224672be
nullgar <technicianaguilar@gmail.com>
2023-01-03 23:24:55 -0800

    Made and example function with string literals

--- TSProject/app.js
+++ TSProject/app.js
@@ -8,12 +8,29 @@ var combine = function (input1, input2) {
     }
     return result;
 };
+var combineWithCheck = function (input1, input2, resultConversion) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + " " + input2.toString();
+    }
+    if (resultConversion === "asNumber") {
+        return result = +input1 + +input2;
+    }
+    else {
+        return result.toString();
+    }
+};
 var theNum1 = 435567;
 var theNum2 = 44522;
 var theStr1 = "Hello";
 var theStr2 = "World!";
 var result = combine(theNum1, theNum2);
 var result2 = combine(theStr1, theStr2);
+var result3 = combineWithCheck(theNum1, theNum2, "asText");
 console.log(result);
 console.log(result2);
+console.log(typeof result3);
 //export{};
--- TSProject/app.ts
+++ TSProject/app.ts
@@ -10,6 +10,24 @@ const combine = (input1: number | string, input2: number | string) => {
     return result;
 };
 
+const combineWithCheck = (input1: number | string, input2: number | string, resultConversion: "asNumber" | "asText") => {
+    let result;
+
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+
+    } else {
+        result = input1.toString() + " " + input2.toString();
+    }
+
+    if (resultConversion === "asNumber") {
+        return result = +input1 + +input2;
+    } else {
+        return result.toString();
+    }
+    
+};
+
 const theNum1 = 435567;
 const theNum2 = 44522;
 const theStr1 = "Hello";
@@ -17,6 +35,8 @@ const theStr2 = "World!"
 
 const result = combine(theNum1, theNum2);
 const result2 = combine(theStr1, theStr2);
+const result3 = combineWithCheck(theNum1, theNum2, "asText");
 console.log(result);
-console.log(result2)
+console.log(result2);
+console.log(typeof result3);
 //export{};
